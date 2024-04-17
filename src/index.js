0ca6000e1b41d874a88e8cd18612d77052ac6cde
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-04-17 17:26:55 -0300

    feat: playRaceEngine(c1, c2)

--- src/index.js
+++ src/index.js
@@ -18,8 +18,18 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+async function playRaceEngine(character1, character2) {
+    for (let round = 1; round <= 5; round++) {
+        console.log(`🏁 Rodada ${round}`);
+        
+        // TODO: Sortear bloco
+    } 
+}
+
 (async function main() {
     console.log(
         `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`
     );
+
+    await playRaceEngine(player1, player2);
 })();
